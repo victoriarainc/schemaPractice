d43017bd658db72cafb4c300208915557324a630
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-08-07 13:50:37 -0400

    created schema in inventory.js, folders to organize future files, and a gitignore to push modules into

---
+++ models/inventory.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// get a reference to Schema
+const Schema = mongoose.Schema;
+
+// create a schema for a contact
+const itemSchema = new Schema({
+  category: { type: String, required: true },
+  size: { type: String },
+  count: { type: String },
+  price: { type: Number, required: true },
+  custom: { type: Boolean, required: true }
+});
+
+// create a model for a Contact
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
